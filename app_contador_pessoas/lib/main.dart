e947868e018ab49dc6c67ee5ae7e5f9dd6bdc08d
Sílvio Ricardo <silviocastro006@gmail.com>
2025-03-18 23:44:09 -0300

    primeiro app

--- app_contador_pessoas/lib/main.dart
+++ app_contador_pessoas/lib/main.dart
@@ -1,91 +1,148 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  int count = 0;
+
   void decrement() {
-    print("Decrement");
+    setState(() {
+      if(count < 0)
+    {
+      count==0;
+    }
+      count--;
+    });
+    print(count);
   }
 
   void increment() {
-    print("Increment");
+    setState(() {
+      if(count > 20)
+    {
+      count==20;
+    }
+
+      count++;
+    });
+    print(count);
   }
+
+  bool get isEmpty => count <= 0;
+  bool get isFull => count >= 20;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Pode entrar!",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
+      backgroundColor: const Color.fromARGB(255, 73, 73, 73),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/imagens/fundo_contador.jpg'),
+            fit: BoxFit.cover,
             ),
-          ),
-          Text(
-            "0",
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100,100),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24)
-                  )
-                ),
-                child: Text(
-                  "Saiu",
-                  style: TextStyle(
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull? "Lotado" : "Pode entrar!",
+              style: TextStyle(
+                fontSize: 60,
+                color: isFull? Colors.red : Colors.white,
+                fontWeight: FontWeight.w700,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(5,5),
+                    blurRadius: 4,
                     color: Colors.black,
-                  ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(32),
+              child: Text(
+                count.toString(),
+                style: TextStyle(
+                  fontSize: 120,
+                  color: Colors.white,
+                  shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(5,5),
+                    blurRadius: 4,
+                    color: Colors.black,
+                  )
+                ],
                 ),
               ),
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100,100),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24)
-                  )
-                ),
-                child: Text(
-                  "Entrou",
-                  style: TextStyle(
-                    color: Colors.black,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty? null : decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isEmpty? Colors.white.withOpacity(0.2) : Colors.white,
+                    foregroundColor: Colors.red,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(width: 50),
+                TextButton(
+                  onPressed: isFull ? null : increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    foregroundColor: Colors.green,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
