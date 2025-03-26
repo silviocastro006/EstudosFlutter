3490b02b078e8a13e11f277964307c15a5986308
Sílvio Ricardo <silviocastro006@gmail.com>
2025-03-25 23:21:19 -0300

    pre finalização primeiro app

--- todo_list/lib/pages/todo_list_page.dart
+++ todo_list/lib/pages/todo_list_page.dart
@@ -1,88 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/widgets/todo_list_item.dart';
+import 'package:todo_list/models/todo.dart';
 
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends StatefulWidget {
   TodoListPage({super.key});
 
   @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<Todo> todos = [];
+
+  final TextEditingController todoController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey)),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromARGB(255, 25, 153, 170))),
-                        floatingLabelStyle:
-                            TextStyle(color: Color.fromARGB(255, 25, 153, 170)),
-                        labelText: 'Adicione uma tarefa',
-                        hintText: 'Ex. Estudar Flutter',
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: todoController,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 25, 153, 170))),
+                          floatingLabelStyle: TextStyle(
+                              color: Color.fromARGB(255, 25, 153, 170)),
+                          labelText: 'Adicione uma tarefa',
+                          hintText: 'Ex. Estudar Flutter',
+                        ),
                       ),
                     ),
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                        onPressed: () {
+                          String text = todoController.text;
+                          setState(() {
+                            Todo newTodo =
+                                Todo(title: text, dateTime: DateTime.now());
+                            todos.add(newTodo);
+                          });
+                          todoController.clear();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 4, 188, 212),
+                          padding: const EdgeInsets.all(12),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          size: 30,
+                          color: Colors.white,
+                        )),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Flexible(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      for (Todo todo in todos)
+                        TodoListItem(
+                          todo: todo,
+                          onDelete: onDelete,
+                        )
+                    ],
                   ),
-                  SizedBox(width: 8),
-                  ElevatedButton(
-                      onPressed: () {},
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child:
+                          Text('Você possui ${todos.length} tarefas pendentes'),
+                    ),
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          todos.clear();
+                        });
+                      },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 4, 188, 212),
+                        backgroundColor: Color.fromARGB(255, 199, 199, 199),
+                        foregroundColor: Colors.white,
                         padding: const EdgeInsets.all(12),
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(5)),
                       ),
-                      child: Icon(
-                        Icons.add,
-                        size: 30,
-                        color: Colors.white,
-                      )),
-                ],
-              ),
-              SizedBox(height: 16),
-              ListView(
-                shrinkWrap: true,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    height: 40,
-                  ),
-                  Container(
-                    color: const Color.fromARGB(255, 216, 174, 171),
-                    height: 40,
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text('Você possui 0 tarefas pendentes'),
-                  ),
-                  SizedBox(width: 8),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 199, 199, 199),
-                      foregroundColor: Colors.white,
-                      padding: const EdgeInsets.all(12),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                    ),
-                    child: Text('Limpar tudo'),
-                  )
-                ],
-              )
-            ],
+                      child: Text('Limpar tudo'),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void onDelete(Todo todo) {
+    setState(() {
+      todos.remove(todo);
+    });
   }
 }
---
+++ todo_list/lib/widgets/todo_list_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_list/models/todo.dart';
+
+class TodoListItem extends StatelessWidget {
+  const TodoListItem({super.key, required this.todo, required this.onDelete,});
+
+  final Todo todo;
+  final Function(Todo) onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      endActionPane: ActionPane(
+        extentRatio: 0.5,
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) => onDelete(todo),
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Deletar',
+          ),
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Color.fromARGB(255, 4, 188, 212),
+            foregroundColor: Colors.white,
+            icon: Icons.edit,
+            label: 'Editar',
+          ),
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: const Color.fromARGB(24, 145, 145, 145),
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 2),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
